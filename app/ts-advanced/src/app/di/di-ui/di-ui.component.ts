60ba837ccb679761e05f2400df7b5e494d71f0fb
lironh <lironenk@gmail.com>
2020-04-04 15:20:50 +0300

    feat(app)

---
+++ app/ts-advanced/src/app/di/di-ui/di-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-di-ui',
+  templateUrl: './di-ui.component.html',
+  styleUrls: ['./di-ui.component.css']
+})
+export class DiUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
