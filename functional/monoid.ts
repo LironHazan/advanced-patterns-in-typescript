df91a691986e229161d61e785edbc99b908f8329
lironh <lironenk@gmail.com>
2020-08-29 09:16:24 +0300

    feat(foldl): little tweak

--- functional/monoid.ts
+++ functional/monoid.ts
@@ -1,20 +1,23 @@
-import {add, BinOp } from "./foldl";
+import { add, BinOp } from './foldl';
 
 interface Monoid<T> {
     id: () => T; // empty
     combine: BinOp //fold/concat etc..
 }
 
-function sumMonoid(): Monoid<number> {
+function getSumMonoid(): Monoid<number> {
     return {
         id: () => 0,
         combine: add
     }
 }
 
-const id = sumMonoid().id();
+const sumMonoid = getSumMonoid();
+console.log(sumMonoid.id());
 // 0
-const sum = sumMonoid().combine(6, 2);
-// 8
+
+console.log(sumMonoid.combine(6, 2));
+console.log(sumMonoid.combine(2, 6) === sumMonoid.combine(6, 2)); // associative
+// true
 
 console.log()
