4a200e0fc247c06c903cae849cce1e688802e867
lironh <lironenk@gmail.com>
2021-05-16 09:23:16 +0300

    feat(utilities): required -?

--- type-system/utilities_ex.ts
+++ type-system/utilities_ex.ts
@@ -1,11 +1,13 @@
-// When the Record type could be handy
+// ADTs
 
+// Product
 interface PageDetail {
   index: number;
+  flag?: boolean;
 }
-
+//Sum
 type Page = 'home' | 'about' | 'contact'; // sum type (home+about+contact)
-
+//  Product
 type Site = Record<Page, PageDetail>; // product type (home*about*concat)
 
 const mySite: Site = {
@@ -19,3 +21,19 @@ type DummyProfilerArgs = Parameters<typeof injectDummyProfiler>;
 type DummyProfilerFN = (...args: DummyProfilerArgs) => void;
 function injectDummyProfiler(filePath: string, className: string, methodName: string): void {}
 
+// Required:
+
+// the interesting part of the Required implementation is --> -?
+// which cancels the optional ?
+// type Required<T> = {
+//   [P in keyof T]-?: T[P];
+// };
+type RPage = Required<PageDetail>;
+const page: PageDetail = {
+  index: 0,
+};
+
+const rpage: RPage = {
+  // Property 'flag' is missing in type '{ index: number; }' but required in type 'Required<PageDetail>'.
+  index: 0,
+};
