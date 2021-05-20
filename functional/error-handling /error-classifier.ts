b2c904f2f6b48ba5a8a08c895d24c0f9f7253f6a
lironh <lironenk@gmail.com>
2021-05-20 07:05:20 +0300

    feat(functional): err handling

---
+++ functional/error-handling /error-classifier.ts
@@ -0,0 +1,73 @@
+// Real life example of a functional I implemented on one of my projects:
+
+export type Result<E, T> = Error<E> | Just<T>;
+type Just<T> = T;
+type Error<E> = Just<E>; //LOL :)
+
+type ANY = any;
+
+export class ResultT<E, T> {
+  private readonly result: Result<ErrorClassification, T>;
+
+  constructor(result: Result<ErrorClassification, T>) {
+    this.result = result;
+  }
+
+  noop(): ResultT<E, T> {
+    return this;
+  }
+
+  /**
+   * Result mapper
+   * @param onSuccessFn
+   */
+  ok(onSuccessFn: (lifted: T) => ResultT<E, T>): ANY | ResultT<E, T> {
+    return isError(this.result) ? this.noop() : onSuccessFn(this.result as T);
+  }
+
+  /**
+   * Error mapper
+   * @param onFailureFn
+   */
+  err(onFailureFn: (error: ErrorClassification) => ResultT<E, T>): ANY | ResultT<E, T> {
+    return isError(this.result) ? onFailureFn(this.result as ErrorClassification) : this.noop();
+  }
+}
+
+export enum ErrorType {
+  ErrOne = 'Unsupported',
+  ErrTwo = 'Invalid',
+}
+
+export interface ErrorClassification {
+  message: string;
+  type: ErrorType;
+}
+
+/**
+ * Since we don't have a type based pattern matching we're using "tags" runtime check
+ * @param result
+ */
+export function isError<T, E extends ErrorClassification>(result: Result<E, T>): result is E {
+  return typeof result === 'object' && 'message' in result && 'type' in result;
+}
+
+// Example
+
+const err: ErrorClassification = {
+  message: 'haha',
+  type: ErrorType.ErrOne,
+};
+
+const mockedErr = new ResultT(err);
+mockedErr
+  .ok(() => mockedErr) // no operation in our case
+  .err((err: ErrorClassification) => {
+    console.log(err.message);
+    err.message = 'transformed err message';
+    return mockedErr;
+  })
+  .err((transformedErr: ErrorClassification) => {
+    console.log(transformedErr.message);
+    return mockedErr;
+  });
